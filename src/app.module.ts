24e51e7f01d2d392cadc76c6cb1cbdec68d11ae1
Saeed Rostami <privatemx.2010@gmail.com>
2023-11-11 17:32:04 +0800

    add users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
 import { ArticlesModule } from './articles/articles.module'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, ArticlesModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, ArticlesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
